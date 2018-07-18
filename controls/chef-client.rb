108d831dc56eda5fc75979d7a7a7869833477365
JJ Asghar <jj@chef.io>
2018-07-18 11:26:27 -0500

    hanging end ;)
    Signed-off-by: JJ Asghar <jj@chef.io>

--- controls/chef-client.rb
+++ controls/chef-client.rb
@@ -2,14 +2,13 @@
 # copyright: 2018, The Authors
 
 title 'chef-client version'
-  control 'chef-client-1.0' do
-    impact 1.0
-    title 'Verify the version of chef-client'
-    desc  '
+control 'chef-client-1.0' do
+  impact 1.0
+  title 'Verify the version of chef-client'
+  desc  '
       This is a profile verifies you are running a modern version of chef client.
     '
-    describe command('chef-client --version') do
-      its('stdout') { should match (/Chef: 1[4-9]/) }
-    end
+  describe command('chef-client --version') do
+    its('stdout') { should match (/Chef: 1[4-9]/) }
   end
 end
